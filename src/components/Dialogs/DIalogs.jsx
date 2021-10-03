3f2e7ea765b20819749b532ab9ff25af1d79d249
Kropachev Evgeniy <enapolskiy@gmail.com>
2021-10-03 14:40:44 +0700

    Dialogs

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -3,35 +3,35 @@ import classes from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
-            <div className={classes.dialogsItem}>
-                <div className={classes.dialog + ' ' + classes.active}>
-                    Dimych
-                </div>
-                <div className={classes.dialog}>
-                    Sergey
-                </div>
-                <div className={classes.dialog}>
-                    Andrey
-                </div>
-                <div className={classes.dialog}>
-                   Viktor
-                </div>
-            </div>
+          <div className={classes.dialogsItem}>
+              <div className={classes.dialog + ' ' + classes.active}>
+                  Dimysh
+              </div>
+              <div className={classes.dialog}>
+                  Sanya
+              </div>
+              <div className={classes.dialog}>
+                  Sonya
+              </div>
+              <div className={classes.dialog}>
+                  Yarik
+              </div>
+          </div>
             <div className={classes.messages}>
                 <div className={classes.message}>
                     Hi!
                 </div>
                 <div className={classes.message}>
-                    How are you?
+                    Hello!
                 </div>
                 <div className={classes.message}>
-                   Yo!
+                   Good morning
                 </div>
                 <div className={classes.message}>
-                    Hello my name is Viktor!
+                    Good afternoon
                 </div>
-
             </div>
+
         </div>
     )
 }
