f5125d4db20ecab7efd2d032f48261230cfbccbe
enapolskiy <enapolskiy83@yandex.ru>
2021-11-28 22:22:56 +0700

    myPostContainer27!

--- src/index.js
+++ src/index.js
@@ -26,9 +26,7 @@ store.subscribe(() =>{
     let state = store.getState();
     _callsubscriber(state)
 })
-// store.subscribe (_callsubscriber)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
